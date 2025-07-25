b993be05a0aeaeefc28c0600e5aa66377a523dec
Elix <119566415+GenericMage@users.noreply.github.com>
2025-07-25 20:30:44 +0100

    Add files via upload

--- Type-A/CCSettlementRouter.sol
+++ Type-A/CCSettlementRouter.sol
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: BSL 1.1 - Peng Protocol 2025
 pragma solidity ^0.8.2;
 
-// Version: 0.0.2
+// Version: 0.0.3
 // Changes:
-// - v0.0.2: Updated settleBuyOrders and settleSellOrders to use Uniswap V2 swaps via _executePartialBuySwap and _executePartialSellSwap, retaining settleBuyLiquid and settleSellLiquid unchanged.
-// - v0.0.1: Created CCSettlementRouter.sol by extracting settleBuyOrders, settleSellOrders, settleBuyLiquid, and settleSellLiquid from SSRouter.sol v0.0.61.
-// - v0.0.1: Imported CCSettlementPartial.sol v0.0.1 for _processBuyOrder, _processSellOrder, executeSingleBuyLiquid, and executeSingleSellLiquid.
-// - v0.0.1: Retained setAgent from SSMainPartial.sol v0.0.25 for contract setup.
-// - v0.0.1: Included SafeERC20 usage and ReentrancyGuard for security.
-// Compatible with SSListingTemplate.sol (v0.0.10), SSLiquidityTemplate.sol (v0.0.6), SSMainPartial.sol (v0.0.25), SSOrderPartial.sol (v0.0.19), SSSettlementPartial.sol (v0.0.58), CCListingTemplate.sol (v0.0.2).
+// - v0.0.3: Replaced ISSListingTemplate with ICCListing from CCMainPartial.sol, updated UpdateType references to ICCListing.UpdateType.
+// - v0.0.2: Updated settleBuyOrders and settleSellOrders to use Uniswap V2 swaps via _executePartialBuySwap and _executePartialSellSwap.
+// - v0.0.1: Created CCSettlementRouter.sol, extracted settlement functions from SSRouter.sol v0.0.61, retained setAgent.
+// Compatible with ICCListing.sol (v0.0.3), ICCLiquidity.sol, CCMainPartial.sol (v0.0.06), CCUniPartial.sol (v0.0.6), CCSettlementPartial.sol (v0.0.16).
 
 import "./utils/CCSettlementPartial.sol";
 
@@ -17,14 +15,14 @@ contract CCSettlementRouter is CCSettlementPartial {
 
     function settleBuyOrders(address listingAddress, uint256 maxIterations) external onlyValidListing(listingAddress) nonReentrant {
         // Settles multiple buy orders up to maxIterations using Uniswap V2 swaps, collecting updates
-        ISSListingTemplate listingContract = ISSListingTemplate(listingAddress);
+        ICCListing listingContract = ICCListing(listingAddress);
         uint256[] memory orderIdentifiers = listingContract.pendingBuyOrdersView();
         uint256 iterationCount = maxIterations < orderIdentifiers.length ? maxIterations : orderIdentifiers.length;
-        ISSListingTemplate.UpdateType[] memory tempUpdates = new ISSListingTemplate.UpdateType[](iterationCount * 2);
+        ICCListing.UpdateType[] memory tempUpdates = new ICCListing.UpdateType[](iterationCount * 2);
         uint256 updateIndex = 0;
         for (uint256 i = 0; i < iterationCount; i++) {
             uint256 orderIdent = orderIdentifiers[i];
-            ISSListingTemplate.UpdateType[] memory updates = _processBuyOrder(listingAddress, orderIdent, listingContract);
+            ICCListing.UpdateType[] memory updates = _processBuyOrder(listingAddress, orderIdent, listingContract);
             if (updates.length == 0) {
                 continue;
             }
@@ -32,7 +30,7 @@ contract CCSettlementRouter is CCSettlementPartial {
                 tempUpdates[updateIndex++] = updates[j];
             }
         }
-        ISSListingTemplate.UpdateType[] memory finalUpdates = new ISSListingTemplate.UpdateType[](updateIndex);
+        ICCListing.UpdateType[] memory finalUpdates = new ICCListing.UpdateType[](updateIndex);
         for (uint256 i = 0; i < updateIndex; i++) {
             finalUpdates[i] = tempUpdates[i];
         }
@@ -43,14 +41,14 @@ contract CCSettlementRouter is CCSettlementPartial {
 
     function settleSellOrders(address listingAddress, uint256 maxIterations) external onlyValidListing(listingAddress) nonReentrant {
         // Settles multiple sell orders up to maxIterations using Uniswap V2 swaps, collecting updates
-        ISSListingTemplate listingContract = ISSListingTemplate(listingAddress);
+        ICCListing listingContract = ICCListing(listingAddress);
         uint256[] memory orderIdentifiers = listingContract.pendingSellOrdersView();
         uint256 iterationCount = maxIterations < orderIdentifiers.length ? maxIterations : orderIdentifiers.length;
-        ISSListingTemplate.UpdateType[] memory tempUpdates = new ISSListingTemplate.UpdateType[](iterationCount * 2);
+        ICCListing.UpdateType[] memory tempUpdates = new ICCListing.UpdateType[](iterationCount * 2);
         uint256 updateIndex = 0;
         for (uint256 i = 0; i < iterationCount; i++) {
             uint256 orderIdent = orderIdentifiers[i];
-            ISSListingTemplate.UpdateType[] memory updates = _processSellOrder(listingAddress, orderIdent, listingContract);
+            ICCListing.UpdateType[] memory updates = _processSellOrder(listingAddress, orderIdent, listingContract);
             if (updates.length == 0) {
                 continue;
             }
@@ -58,7 +56,7 @@ contract CCSettlementRouter is CCSettlementPartial {
                 tempUpdates[updateIndex++] = updates[j];
             }
         }
-        ISSListingTemplate.UpdateType[] memory finalUpdates = new ISSListingTemplate.UpdateType[](updateIndex);
+        ICCListing.UpdateType[] memory finalUpdates = new ICCListing.UpdateType[](updateIndex);
         for (uint256 i = 0; i < updateIndex; i++) {
             finalUpdates[i] = tempUpdates[i];
         }
@@ -69,20 +67,20 @@ contract CCSettlementRouter is CCSettlementPartial {
 
     function settleBuyLiquid(address listingAddress, uint256 maxIterations) external onlyValidListing(listingAddress) nonReentrant {
         // Settles multiple buy order liquidations up to maxIterations
-        ISSListingTemplate listingContract = ISSListingTemplate(listingAddress);
+        ICCListing listingContract = ICCListing(listingAddress);
         uint256[] memory orderIdentifiers = listingContract.pendingBuyOrdersView();
         uint256 iterationCount = maxIterations < orderIdentifiers.length ? maxIterations : orderIdentifiers.length;
-        ISSListingTemplate.UpdateType[] memory tempUpdates = new ISSListingTemplate.UpdateType[](iterationCount * 2);
+        ICCListing.UpdateType[] memory tempUpdates = new ICCListing.UpdateType[](iterationCount * 2);
         uint256 updateIndex = 0;
         for (uint256 i = 0; i < iterationCount; i++) {
-            ISSListingTemplate.UpdateType[] memory updates = executeSingleBuyLiquid(listingAddress, orderIdentifiers[i]);
+            ICCListing.UpdateType[] memory updates = executeSingleBuyLiquid(listingAddress, orderIdentifiers[i]);
             if (updates.length > 0) {
                 for (uint256 j = 0; j < updates.length; j++) {
                     tempUpdates[updateIndex++] = updates[j];
                 }
             }
         }
-        ISSListingTemplate.UpdateType[] memory finalUpdates = new ISSListingTemplate.UpdateType[](updateIndex);
+        ICCListing.UpdateType[] memory finalUpdates = new ICCListing.UpdateType[](updateIndex);
         for (uint256 i = 0; i < updateIndex; i++) {
             finalUpdates[i] = tempUpdates[i];
         }
@@ -93,20 +91,20 @@ contract CCSettlementRouter is CCSettlementPartial {
 
     function settleSellLiquid(address listingAddress, uint256 maxIterations) external onlyValidListing(listingAddress) nonReentrant {
         // Settles multiple sell order liquidations up to maxIterations
-        ISSListingTemplate listingContract = ISSListingTemplate(listingAddress);
+        ICCListing listingContract = ICCListing(listingAddress);
         uint256[] memory orderIdentifiers = listingContract.pendingSellOrdersView();
         uint256 iterationCount = maxIterations < orderIdentifiers.length ? maxIterations : orderIdentifiers.length;
-        ISSListingTemplate.UpdateType[] memory tempUpdates = new ISSListingTemplate.UpdateType[](iterationCount * 2);
+        ICCListing.UpdateType[] memory tempUpdates = new ICCListing.UpdateType[](iterationCount * 2);
         uint256 updateIndex = 0;
         for (uint256 i = 0; i < iterationCount; i++) {
-            ISSListingTemplate.UpdateType[] memory updates = executeSingleSellLiquid(listingAddress, orderIdentifiers[i]);
+            ICCListing.UpdateType[] memory updates = executeSingleSellLiquid(listingAddress, orderIdentifiers[i]);
             if (updates.length > 0) {
                 for (uint256 j = 0; j < updates.length; j++) {
                     tempUpdates[updateIndex++] = updates[j];
                 }
             }
         }
-        ISSListingTemplate.UpdateType[] memory finalUpdates = new ISSListingTemplate.UpdateType[](updateIndex);
+        ICCListing.UpdateType[] memory finalUpdates = new ICCListing.UpdateType[](updateIndex);
         for (uint256 i = 0; i < updateIndex; i++) {
             finalUpdates[i] = tempUpdates[i];
         }
