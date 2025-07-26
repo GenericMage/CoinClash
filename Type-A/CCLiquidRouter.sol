35e62f99b314a88e797669f684cde20a61de21c2
Elix <119566415+GenericMage@users.noreply.github.com>
2025-07-26 09:31:58 +0100

    Add files via upload

---
+++ Type-A/CCLiquidRouter.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: BSL 1.1 - Peng Protocol 2025
+pragma solidity ^0.8.2;
+
+// Version: 0.0.1
+// Changes:
+// - v0.0.1: Created CCLiquidRouter.sol, extracted settleBuyLiquid and settleSellLiquid from CCSettlementRouter.sol.
+// Compatible with ICCListing.sol (v0.0.3), ICCLiquidity.sol, CCMainPartial.sol (v0.0.06), CCLiquidPartial.sol (v0.0.1).
+
+import "./CCLiquidPartial.sol";
+
+contract CCLiquidRouter is CCLiquidPartial {
+    using SafeERC20 for IERC20;
+
+    function settleBuyLiquid(address listingAddress, uint256 maxIterations) external onlyValidListing(listingAddress) nonReentrant {
+        // Settles multiple buy order liquidations up to maxIterations
+        ICCListing listingContract = ICCListing(listingAddress);
+        uint256[] memory orderIdentifiers = listingContract.pendingBuyOrdersView();
+        uint256 iterationCount = maxIterations < orderIdentifiers.length ? maxIterations : orderIdentifiers.length;
+        ICCListing.UpdateType[] memory tempUpdates = new ICCListing.UpdateType[](iterationCount * 3);
+        uint256 updateIndex = 0;
+        for (uint256 i = 0; i < iterationCount; i++) {
+            ICCListing.UpdateType[] memory updates = executeSingleBuyLiquid(listingAddress, orderIdentifiers[i]);
+            if (updates.length > 0) {
+                for (uint256 j = 0; j < updates.length; j++) {
+                    tempUpdates[updateIndex++] = updates[j];
+                }
+            }
+        }
+        ICCListing.UpdateType[] memory finalUpdates = new ICCListing.UpdateType[](updateIndex);
+        for (uint256 i = 0; i < updateIndex; i++) {
+            finalUpdates[i] = tempUpdates[i];
+        }
+        if (updateIndex > 0) {
+            listingContract.update(address(this), finalUpdates);
+        }
+    }
+
+    function settleSellLiquid(address listingAddress, uint256 maxIterations) external onlyValidListing(listingAddress) nonReentrant {
+        // Settles multiple sell order liquidations up to maxIterations
+        ICCListing listingContract = ICCListing(listingAddress);
+        uint256[] memory orderIdentifiers = listingContract.pendingSellOrdersView();
+        uint256 iterationCount = maxIterations < orderIdentifiers.length ? maxIterations : orderIdentifiers.length;
+        ICCListing.UpdateType[] memory tempUpdates = new ICCListing.UpdateType[](iterationCount * 3);
+        uint256 updateIndex = 0;
+        for (uint256 i = 0; i < iterationCount; i++) {
+            ICCListing.UpdateType[] memory updates = executeSingleSellLiquid(listingAddress, orderIdentifiers[i]);
+            if (updates.length > 0) {
+                for (uint256 j = 0; j < updates.length; j++) {
+                    tempUpdates[updateIndex++] = updates[j];
+                }
+            }
+        }
+        ICCListing.UpdateType[] memory finalUpdates = new ICCListing.UpdateType[](updateIndex);
+        for (uint256 i = 0; i < updateIndex; i++) {
+            finalUpdates[i] = tempUpdates[i];
+        }
+        if (updateIndex > 0) {
+            listingContract.update(address(this), finalUpdates);
+        }
+    }
+}
