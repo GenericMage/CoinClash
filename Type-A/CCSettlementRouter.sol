eff9f66b44ab6800db351bdda82253a622b22075
Elix <119566415+GenericMage@users.noreply.github.com>
2025-07-26 10:00:31 +0100

    Add files via upload

--- Type-A/CCSettlementRouter.sol
+++ Type-A/CCSettlementRouter.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: BSL 1.1 - Peng Protocol 2025
 pragma solidity ^0.8.2;
 
-// Version: 0.0.3
+// Version: 0.0.4
 // Changes:
+// - v0.0.4: Removed liquid settlement functions (settleBuyLiquid, settleSellLiquid) and dependencies (_prepBuyLiquidUpdates, _prepSellLiquidUpdates, executeSingleBuyLiquid, executeSingleSellLiquid, _prepareLiquidityTransaction, _checkAndTransferPrincipal, _updateLiquidity) from CCSettlementPartial.sol, removed ICCLiquidity interface, retained Uniswap V2 settlement.
 // - v0.0.3: Replaced ISSListingTemplate with ICCListing from CCMainPartial.sol, updated UpdateType references to ICCListing.UpdateType.
 // - v0.0.2: Updated settleBuyOrders and settleSellOrders to use Uniswap V2 swaps via _executePartialBuySwap and _executePartialSellSwap.
 // - v0.0.1: Created CCSettlementRouter.sol, extracted settlement functions from SSRouter.sol v0.0.61, retained setAgent.
-// Compatible with ICCListing.sol (v0.0.3), ICCLiquidity.sol, CCMainPartial.sol (v0.0.06), CCUniPartial.sol (v0.0.6), CCSettlementPartial.sol (v0.0.16).
+// Compatible with ICCListing.sol (v0.0.3), CCMainPartial.sol (v0.0.06), CCUniPartial.sol (v0.0.6).
 
 import "./utils/CCSettlementPartial.sol";
 
@@ -54,54 +55,6 @@ contract CCSettlementRouter is CCSettlementPartial {
             }
             for (uint256 j = 0; j < updates.length; j++) {
                 tempUpdates[updateIndex++] = updates[j];
-            }
-        }
-        ICCListing.UpdateType[] memory finalUpdates = new ICCListing.UpdateType[](updateIndex);
-        for (uint256 i = 0; i < updateIndex; i++) {
-            finalUpdates[i] = tempUpdates[i];
-        }
-        if (updateIndex > 0) {
-            listingContract.update(address(this), finalUpdates);
-        }
-    }
-
-    function settleBuyLiquid(address listingAddress, uint256 maxIterations) external onlyValidListing(listingAddress) nonReentrant {
-        // Settles multiple buy order liquidations up to maxIterations
-        ICCListing listingContract = ICCListing(listingAddress);
-        uint256[] memory orderIdentifiers = listingContract.pendingBuyOrdersView();
-        uint256 iterationCount = maxIterations < orderIdentifiers.length ? maxIterations : orderIdentifiers.length;
-        ICCListing.UpdateType[] memory tempUpdates = new ICCListing.UpdateType[](iterationCount * 2);
-        uint256 updateIndex = 0;
-        for (uint256 i = 0; i < iterationCount; i++) {
-            ICCListing.UpdateType[] memory updates = executeSingleBuyLiquid(listingAddress, orderIdentifiers[i]);
-            if (updates.length > 0) {
-                for (uint256 j = 0; j < updates.length; j++) {
-                    tempUpdates[updateIndex++] = updates[j];
-                }
-            }
-        }
-        ICCListing.UpdateType[] memory finalUpdates = new ICCListing.UpdateType[](updateIndex);
-        for (uint256 i = 0; i < updateIndex; i++) {
-            finalUpdates[i] = tempUpdates[i];
-        }
-        if (updateIndex > 0) {
-            listingContract.update(address(this), finalUpdates);
-        }
-    }
-
-    function settleSellLiquid(address listingAddress, uint256 maxIterations) external onlyValidListing(listingAddress) nonReentrant {
-        // Settles multiple sell order liquidations up to maxIterations
-        ICCListing listingContract = ICCListing(listingAddress);
-        uint256[] memory orderIdentifiers = listingContract.pendingSellOrdersView();
-        uint256 iterationCount = maxIterations < orderIdentifiers.length ? maxIterations : orderIdentifiers.length;
-        ICCListing.UpdateType[] memory tempUpdates = new ICCListing.UpdateType[](iterationCount * 2);
-        uint256 updateIndex = 0;
-        for (uint256 i = 0; i < iterationCount; i++) {
-            ICCListing.UpdateType[] memory updates = executeSingleSellLiquid(listingAddress, orderIdentifiers[i]);
-            if (updates.length > 0) {
-                for (uint256 j = 0; j < updates.length; j++) {
-                    tempUpdates[updateIndex++] = updates[j];
-                }
             }
         }
         ICCListing.UpdateType[] memory finalUpdates = new ICCListing.UpdateType[](updateIndex);
